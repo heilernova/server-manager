88ea344dc9185a5b0d6b028bb63cadcba097f686
Heiler Nova <heilernova@outlook.com>
2024-01-09 09:47:44 -0500

    Change config initial

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+import { AppModule } from '@api/app.module';
+import { responseMiddleware } from '@api/middlewares';
 
 async function bootstrap() {
+  dotenv.config({ path: '.env' })
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors();
+  app.use(responseMiddleware);
+
+  try {
+    const port = process.env.PORT ?? 3000;
+    await app.listen(port);
+    console.log(`[APP] running in port ${port}`)
+  } catch (error) {
+    console.log(`[Error] Al iniciar la aplicación: `, error.message);
+    process.exit(1);
+  }
 }
 bootstrap();
