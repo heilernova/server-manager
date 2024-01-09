0b5c9a94ae195831efc92b35256e6d550d896674
Heiler Nova <heilernova@outlook.com>
2024-01-09 15:33:12 -0500

    Change response

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@api/common/config/config.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly _config: ConfigService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return {
+      name: 'Server manager For Novaccode',
+      version: this._config.version,
+      env: this._config.nodeEnv,
+      panel: 'https://smpanel.novaccode.com',
+      urls: [
+        'https://www.novaccode.com',
+        'https://smpanel.novaccode.com'
+      ]
+    }
   }
 }
