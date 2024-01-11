65781f0949306a0fe7279c8c1f23c6d46d97be20
Heiler Nova <heilernova@outlook.com>
2024-01-11 09:32:09 -0500

    Add function for update password

--- apps/api/src/controllers/profile/profile.controller.ts
+++ apps/api/src/controllers/profile/profile.controller.ts
@@ -1,6 +1,6 @@
 import { DbUsersService, IUser } from '@api/common/database';
 import { GetSession, IsLoggedInGuard } from '@api/common/session';
-import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, Patch, UseGuards } from '@nestjs/common';
 import { ProfileUpdateDto } from './dto/profile-update.dto';
 
 @UseGuards(IsLoggedInGuard)
@@ -24,5 +24,12 @@ export class ProfileController {
     @Patch()
     async update(@GetSession() session: IUser, @Body() body: ProfileUpdateDto){
         await this._dbUsers.update(session.id, body);
+    }
+
+    @Patch('password')
+    async password(@GetSession() session: IUser, @Body() body: any){
+        const passwordValid: boolean = await this._dbUsers.passwordValid(session.id, body.currentPassword);
+        if (!passwordValid) throw new HttpException("Tu contraseña es incorrecta", 400);
+        await this._dbUsers.update(session.id, { password: body.newPassword });
     }
 }
