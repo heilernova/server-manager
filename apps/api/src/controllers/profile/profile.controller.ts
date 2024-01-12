728fc61b9040ef108278dc62f080ccf733041d94
Heiler Nova <heilernova@outlook.com>
2024-01-12 10:15:57 -0500

    Add http code 204

--- apps/api/src/controllers/profile/profile.controller.ts
+++ apps/api/src/controllers/profile/profile.controller.ts
@@ -1,6 +1,6 @@
 import { DbUsersService, IUser } from '@api/common/database';
 import { GetSession, IsLoggedInGuard } from '@api/common/session';
-import { Body, Controller, Get, HttpException, Patch, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpException, Patch, UseGuards } from '@nestjs/common';
 import { ProfileUpdateDto } from './dto/profile-update.dto';
 import { UpdatePasswordDto } from './dto/update-password.dto';
 
@@ -23,11 +23,13 @@ export class ProfileController {
     }
 
     @Patch()
+    @HttpCode(204)
     async update(@GetSession() session: IUser, @Body() body: ProfileUpdateDto){
         await this._dbUsers.update(session.id, body);
     }
 
     @Patch('password')
+    @HttpCode(204)
     async password(@GetSession() session: IUser, @Body() body: UpdatePasswordDto){
         const passwordValid: boolean = await this._dbUsers.passwordValid(session.id, body.currentPassword);
         if (!passwordValid) throw new HttpException("Tu contraseña es incorrecta", 400);
