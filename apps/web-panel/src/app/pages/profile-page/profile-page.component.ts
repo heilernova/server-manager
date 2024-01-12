cb83f50ecfa55274d96e60a9cd326cefa162fbd6
Heiler Nova <heilernova@outlook.com>
2024-01-12 09:01:42 -0500

    Load info profile in form

--- apps/web-panel/src/app/pages/profile-page/profile-page.component.ts
+++ apps/web-panel/src/app/pages/profile-page/profile-page.component.ts
@@ -5,11 +5,14 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzModalModule, NzModalService } from 'ng-zorro-antd/modal';
 import { ModalUpdatePasswordComponent } from '@app/components/modal-update-password/modal-update-password.component';
+import { ApiProfileService } from '@app/common/api/profile';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
   imports: [
+    ReactiveFormsModule,
     NzButtonModule,
     NzFormModule,
     NzInputModule,
@@ -20,6 +23,32 @@ import { ModalUpdatePasswordComponent } from '@app/components/modal-update-passw
 })
 export class ProfilePageComponent {
   private readonly _modalService = inject(NzModalService);
+  private readonly _apiProfile = inject(ApiProfileService);
+
+  public readonly formGroup = new FormGroup({
+    name: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
+    lastName: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
+    username: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
+    email: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
+    cellphone: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
+  })
+
+  constructor(){
+    this._apiProfile.getInfo().subscribe({
+      next: res => {
+        this.formGroup.setValue({
+          name: res.name,
+          lastName: res.lastName,
+          username: res.username,
+          email: res.email,
+          cellphone: res.cellphone
+        })
+      },
+      error: err => {
+        console.log(err);
+      }
+    })
+  }
 
   onClickUpdatePassword(){
     this._modalService.create({
