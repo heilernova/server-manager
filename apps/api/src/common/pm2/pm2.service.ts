19e1bbf541efd64c65b864b876eff140a52d493f
Heiler Nova <heilernov@outlook.com>
2024-01-15 11:23:17 -0500

    Update Pm2Service

--- apps/api/src/common/pm2/pm2.service.ts
+++ apps/api/src/common/pm2/pm2.service.ts
@@ -1,5 +1,6 @@
 import { execSync } from 'node:child_process';
 import { Injectable } from '@nestjs/common';
+import { uuid } from '@api/common/database';
 
 @Injectable()
 export class Pm2Service {
@@ -8,20 +9,24 @@ export class Pm2Service {
         return JSON.parse(result.toString());
     }
 
-    start(path: string, script: string, name: string, env: { [key: string]: string }) {
-        execSync(`pm2 start ${script} --name="${name}"`, { cwd: path, env: env as any });
+    start(path: string, script: string, id: uuid, env: { [key: string]: string }) {
+        execSync(`pm2 start ${script} --name="${id}"`, { cwd: path, env: env as any });
     }
 
-    stop(name: string){
-        execSync(`pm2 stop ${name}`);
+    stop(id: uuid){
+        execSync(`pm2 stop ${id}`);
     }
 
-    reload(name: string, env?: { [key: string]: string }){
+    reload(id: uuid, env?: { [key: string]: string }){
         if (env){
-            execSync(`pm2 reload ${name} --update-env`, { env: env as any });
+            execSync(`pm2 reload ${id} --update-env`, { env: env as any });
         } else {
-            execSync(`pm2 reload ${name}`);
+            execSync(`pm2 reload ${id}`);
         }
+    }
+
+    delete(id: uuid){
+        execSync(`pm2 delete ${id}`);
     }
 }
 
