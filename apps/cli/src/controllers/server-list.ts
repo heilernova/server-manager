a3afb38bb02bf3cb0e9cd2925fd972aac1572a43
Heiler Nova <heilernov@outlook.com>
2024-02-05 10:12:21 -0500

    Add controller serverListController

---
+++ apps/cli/src/controllers/server-list.ts
@@ -0,0 +1,16 @@
+import { inject } from "../core.js";
+
+export const serverListController = () => {
+    const config = inject.config();
+    if (config.servers.length > 0){
+        console.table(config.servers.map(x => {
+            return {
+                url: x.url,
+                username: x.username,
+                authentication: x.authentication.type == 'key' ? 'APP-KEY' :  'JWT'
+            }
+        }))
+    } else {
+        console.log("No servers");
+    }
+}
